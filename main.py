d18c9eee27bbf27dc9ebf79c7aeabb8d28d299c8
queredda <mahsaqueredabahjah@mail.ugm.ac.id>
2025-04-18 14:32:18 +0700

    benerin streamlit

--- main.py
+++ main.py
@@ -7,23 +7,27 @@ app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-@app.route('/upload', methods=['POST'])
+
+@app.route("/upload", methods=["POST"])
 def upload():
     now = datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = f"image_{now}.jpg"
     filepath = os.path.join(UPLOAD_FOLDER, filename)
-    with open(filepath, 'wb') as f:
+    with open(filepath, "wb") as f:
         f.write(request.data)
     print(f"Saved {filename}")
     return "Image received!", 200
 
-@app.route('/')
+
+@app.route("/")
 def gallery():
     files = os.listdir(UPLOAD_FOLDER)
-    images_html = ''.join(
-        f'<div><img src="/uploads/{file}" width="400"><p>{file}</p></div>' for file in sorted(files, reverse=True)
+    images_html = "".join(
+        f'<div><img src="/uploads/{file}" width="400"><p>{file}</p></div>'
+        for file in sorted(files, reverse=True)
     )
-    return render_template_string(f"""
+    return render_template_string(
+        f"""
     <html>
       <head><title>ESP32-CAM Gallery</title></head>
       <body>
@@ -31,21 +35,24 @@ def gallery():
         {images_html}
       </body>
     </html>
-    """)
+    """
+    )
 
-@app.route('/uploads/<filename>')
+
+@app.route("/uploads/<filename>")
 def serve_image(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
-@app.route('/verifystudents', methods=['POST'])
+
+@app.route("/verifystudents", methods=["POST"])
 def verify():
     now = datetime.now().strftime("%Y%m%d_%H%M%S")
     uploaded_img = f"image_{now}.jpg"
     uploaded_img = os.path.join(UPLOAD_FOLDER, uploaded_img)
-    with open(uploaded_img, 'wb') as f:
+    with open(uploaded_img, "wb") as f:
         f.write(request.data)
 
-    stud_id = request.headers.get('X-UID')
+    stud_id = request.headers.get("X-UID")
 
     if not uploaded_img or not stud_id:
         return jsonify({"error": "Missing 'img' or 'studId'"}), 400
@@ -54,10 +61,11 @@ def verify():
     print(result)
     return jsonify({"verified": result}), 200
 
+
 # TUGAS IJAL
 # bikin api endpoint atau fungsi yang ngedetsi kalo udah tgl ex. 1
 # kirim recap ke gmail orang tua
 # bikin model user = [nama, rfid, gmail orang tua]
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
